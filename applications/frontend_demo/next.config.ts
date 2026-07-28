a903d8dda446168bb509ce5bbe6c269e18c97dd0
RanieryAV <ranieryvasconcelos@gmail.com>
2026-07-28 14:29:52 -0300

    fix: added more possible Docker IPs to allowedDevOrigins in the 'next.config.js' file

--- applications/frontend_demo/next.config.ts
+++ applications/frontend_demo/next.config.ts
@@ -4,7 +4,9 @@ const nextConfig = {
     // Allow the Docker IP so Hot-Reload (HMR) and dynamic component loading (such as the map) work without being blocked.
     allowedDevOrigins: [
         '172.18.0.11',
-        '172.19.0.11', 
+        '172.19.0.11',
+        '172.18.0.12',
+        '172.19.0.12', 
         'localhost'
     ]
 };

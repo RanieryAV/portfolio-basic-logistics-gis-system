85836b7c2dded6b371e23dfd85f3d6c80867a6fb
RanieryAV <ranieryvasconcelos@gmail.com>
2026-08-18 14:06:30 -0300

    refact: added back dockerfiles

--- applications/frontend_demo/next.config.ts
+++ applications/frontend_demo/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  // Allow the Docker IP so Hot-Reload (HMR) and dynamic component loading (such as the map) work without being blocked.
+    allowedDevOrigins: [
+        '172.18.0.11',
+        '172.19.0.11',
+        '172.18.0.12',
+        '172.19.0.12',
+        '192.168.0.8',
+        'localhost'
+    ]
 };
 
 export default nextConfig;
